dd9c47cec484fa3a27a27c546c4549250091dfcb
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-27 19:37:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,10 @@ class Student < User
   attr_accessor :knowledge
 
   def initialize
-    @knowledge = []
+    KNOWLEDGE = []
   end
   
-  def learn(knowledge_string)
-    @knowledge << knowledge_string
-  end
+ 
   
   
     
